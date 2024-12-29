e14900a8097c27b14d4c07b5d5034da09cc964e3
swati <swati@SWATI>
2024-12-29 17:52:29 +0530

    Add project files.

---
+++ Assignment2_strings/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace Assignment2_strings
+{
+    public class Program
+    {
+
+        //Count no. of characters in the sentence
+
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Give the sentence");
+            String sentence = Console.ReadLine();
+            StringMethods sm = new StringMethods();
+            int choice;
+            do
+            {
+                Console.WriteLine("\n*** Menu ***");
+                Console.WriteLine("1.Count no. of characters in the sentence ");
+                Console.WriteLine("2. Count no. of words in a sentence");
+                Console.WriteLine("3. Reverse a sentence");
+                Console.WriteLine("4. Check whether the sentence is palindrome or not");
+                Console.WriteLine("5. Count number of vowels, consonants , integers and special characters in the sentence");
+                Console.WriteLine("6. Exit");
+                Console.Write("Enter your choice: ");
+
+                if (int.TryParse(Console.ReadLine(), out choice))
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.WriteLine("You chose Option 1.");
+
+                            sm.Countcharacters(sentence);
+                            break;
+                        case 2:
+                            Console.WriteLine("You chose Option 2.");
+                            sm.Countwords(sentence);
+                            break;
+                        case 3:
+                            Console.WriteLine("You chose Option 3");
+                            sm.Reverse(sentence);
+                            break;
+                        case 4:
+                            Console.WriteLine("You chose Option 4.");
+                            sm.Palindrome(sentence);
+                            break;
+                        case 5:
+                            Console.WriteLine("You chose Option 5");
+                            sm.CountSentence(sentence);
+                            break;
+                            break;
+                        default:
+                            Console.WriteLine("Invalid choice. Please try again.");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a number.");
+                    choice = 0; // Reset to continue loop
+                }
+
+
+            }
+            while (choice != 6);
+
+        }
+    }
+}
---
+++ Assignment2_strings/StringMethods.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2_strings
+{
+    public class StringMethods
+    {
+        //Count no. of characters in the sentence
+        public int Countcharacters(String sentence)
+        {
+            int len = sentence.Replace(" ", "").Length;
+            Console.WriteLine($"the number of character in sentence is : {len}");
+            return len;
+        }
+
+        //Count no. of words in a sentence
+        public void Countwords(String sentence)
+        {
+            string[] words = sentence.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine($"the number of words in sentence is : {words.Length}");
+        }
+
+        //Reverse a sentence
+        public void Reverse(String sentence)
+        {
+            string[] words = sentence.Split(' ');
+            for (int i = words.Length - 1; i >= 0; i--)
+            {
+                Console.Write(words[i] + " ");
+            }
+        }
+        //Check whether the sentence is palindrome or not
+        public void Palindrome(String sentence)
+        {
+            string[] words = sentence.Split(' ');
+            if (words.SequenceEqual(words.Reverse()))
+            {
+                Console.WriteLine("It is a palindrome");
+            }
+            else
+            {
+                Console.WriteLine("Not a palindrome");
+            }
+        }
+
+        //Count number of vowels, consonants , integers and special characters in the sentence.
+        //sathwika
+        public void CountSentence(String sentence)
+        {
+            int vowels = 0, consonants = 0, digits = 0, specialCharacters = 0;
+            foreach (char c in sentence)
+            {
+                if ("aeiouAEIOU".Contains(sentence))
+                {
+
+
+                    vowels++;
+
+                }
+                else if (Char.IsLetter(c))//consonant
+                {
+
+                    consonants++;
+
+                }
+                else if (Char.IsDigit(c))//digit
+                {
+                    Console.WriteLine("\nThe character is an integer.");
+                    digits++;
+                }
+                else
+                {
+                    Console.WriteLine("\nThe character is a special character.");
+                    specialCharacters++;
+                }
+
+            }
+            Console.WriteLine($"Vowels: {vowels}");
+            Console.WriteLine($"Consonants: {consonants}");
+            Console.WriteLine($"Digits: {digits}");
+            Console.WriteLine($"Special Characters: {specialCharacters}");
+
+        }
+
+
+    }
+}
+
